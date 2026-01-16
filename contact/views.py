70930b489529f424b271e5128b5a3499800da448
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-01-16 14:36:20 -0500

    Update views.py

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,4 @@
+import json
 from rest_framework.decorators import api_view, parser_classes
 from rest_framework.parsers import JSONParser, FormParser, MultiPartParser
 from rest_framework.response import Response
@@ -8,15 +9,27 @@ from django.conf import settings
 @api_view(["POST"])
 @parser_classes([JSONParser, FormParser, MultiPartParser])
 def contact_view(request):
-    name = request.data.get("from_name")
-    email = request.data.get("from_email")
-    message = request.data.get("message")
+    data = request.data
+
+    # 🔥 CASO ESPECIAL RENDER
+    if "_content" in data:
+        try:
+            data = json.loads(data["_content"])
+        except json.JSONDecodeError:
+            return Response(
+                {"error": "JSON inválido"},
+                status=400
+            )
+
+    name = data.get("from_name")
+    email = data.get("from_email")
+    message = data.get("message")
 
     if not name or not email or not message:
         return Response(
             {
                 "error": "Todos los campos son obligatorios",
-                "data_recibida": request.data
+                "data_recibida": data
             },
             status=400
         )
