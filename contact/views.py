d46ad0bddd5d5ac49111ce4179c2a40d29eae5ad
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-01-16 15:16:10 -0500

    Update views.py

--- contact/views.py
+++ contact/views.py
@@ -1,15 +1,15 @@
 from rest_framework.decorators import api_view, parser_classes
 from rest_framework.response import Response
-from rest_framework.parsers import JSONParser
+from rest_framework.parsers import JSONParser, FormParser, MultiPartParser
 from django.core.mail import send_mail
 from django.conf import settings
 
 
 @api_view(["POST"])
-@parser_classes([JSONParser])
+@parser_classes([JSONParser, FormParser, MultiPartParser])
 def contact_view(request):
 
-    print("BODY:", request.body)
+    print("CONTENT TYPE:", request.content_type)
     print("DATA:", request.data)
 
     name = request.data.get("from_name")
